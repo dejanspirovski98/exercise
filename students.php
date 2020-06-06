9f4d020d9269341efbb745722b0c2bc3b07786ab
dejanspirovski <66472001+dejanspirovski@users.noreply.github.com>
2020-06-06 17:00:45 +0200

    Update students.php

--- students.php
+++ students.php
@@ -68,7 +68,7 @@
 		}
 		$servername = "localhost";
 		$username = "root";
-		$password = "karpos12345";
+		$password = "password";
 		$dbname = "students";
 		$conn = new mysqli($servername, $username, $password, $dbname);
 		if ($conn->connect_error) {
